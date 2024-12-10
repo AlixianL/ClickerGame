aced5e18e163f9745fe8c079c5f37ac9c7ac7860
Esma-AlixianL <a.louraco@rns.ecolescreatives.com>
2024-12-10 14:02:54 +0100

    v4
    ajout du script dans le jeux principale + animation et sound design

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -14,22 +14,6 @@ public class Game : MonoBehaviour
     public float x;
 
 
-    public int shop1prize;
-    public TMP_Text shop1text;
-
-    public int shop2prize;
-    public TMP_Text shop2text;
-
-    public TMP_Text amount1Text;
-    public int amount1;
-    public float amount1Profit;
-
-    public TMP_Text amount2Text;
-    public int amount2;
-    public float amount2Profit;
-
-    public int upgradePrize;
-    public TMP_Text upgradeText;
 
 
 
@@ -48,57 +32,11 @@ public class Game : MonoBehaviour
         scoreIncreasedPerSecond = x * Time.deltaTime;
         currentScore = currentScore + scoreIncreasedPerSecond;
 
-        shop1text.text = "Tier 1:" + shop1prize + "$";
-        shop2text.text = "Tier 2:" + shop2prize + "$";
-
-        amount1Text.text = " Tier 1:" + amount1 + "arts $:" + amount1Profit + "/s";
-        amount2Text.text = " Tier 2:" + amount2 + "arts $:" + amount2Profit + "/s";
-
-        upgradeText.text = "Cost :" + upgradePrize + " $";
-
-
-
-
     }
 
     public void Hit()
     {
         currentScore += hitPower;
-    }
-
-    public void Shop1()
-    {
-        if(currentScore>=shop1prize)
-        {
-            currentScore -= shop1prize;
-            amount1 += 1;
-            amount1Profit += 1;
-            x += 10;
-            shop1prize += 25;
-        }
-    }
-
-
-    public void Shop2()
-    {
-        if (currentScore >= shop2prize)
-        {
-            currentScore -= shop2prize;
-            amount2 += 1;
-            amount2Profit += 1;
-            x += 1000000;
-            shop2prize += 100;
-        }
-    }
-
-    public void Upgrade()
-    {
-        if(currentScore>=upgradePrize)
-        {
-            currentScore -= upgradePrize;
-            hitPower *= 2;
-            upgradePrize *= 3;
-        }
     }
 
 }
